d754338c4aaa8e223c5d4aff69f7f8782cce0d92
AlexTroiRu <ASTroiRu@yandex.ru>
2024-09-13 22:09:40 +0300

    second

--- main.go
+++ main.go
@@ -3,8 +3,11 @@ package main
 import "fmt"
 
 func main() {
-	iq := 120
-	if iq == 120 {
-		fmt.Println("Вы гений!")
-	}
+	l := [11]string{"Ноль", "Один", "Два", "Три", "Четыре", "Пять",
+		"Шесть", "Семь", "Восемь", "Девять", "Десять"}
+	var n1, n2, n3 int
+	fmt.Scan(&n1, &n2, &n3)
+	fmt.Println(l[n1])
+	fmt.Println(l[n2])
+	fmt.Println(l[n3])
 }
