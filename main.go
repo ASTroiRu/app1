0e7385c72345f334c7895e297093c6e7648ece37
AlexTroiRu <ASTroiRu@yandex.ru>
2024-09-18 21:44:06 +0300

    операции со строками(парсим, вывод по числам, конкатенация строк)

--- main.go
+++ main.go
@@ -1,33 +1,22 @@
 package main
 
-import "fmt"
-
-func download(size int, ch chan int) {
-	res := 0
-	for i := 0; i <= size; i++ {
-		res += i
-	}
-	ch <- res
-}
-
-func mult(a, b int) int {
-	return a * b
-}
+import (
+	"fmt"
+	"strconv"
+)
 
 func main() {
-	ch1 := make(chan int)
-	ch2 := make(chan int)
-	ch3 := make(chan int)
+	var num, del int
+	fmt.Scan(&num, &del)
+	var rezString string
 
-	var s1, s2, s3 int
-	fmt.Scanln(&s1)
-	fmt.Scanln(&s2)
-	fmt.Scanln(&s3)
+	stringNum := strconv.Itoa(num)
+	for _, v := range stringNum {
+		tempDigit, _ := strconv.Atoi(string(v))
+		if tempDigit != del {
+			rezString += strconv.Itoa(tempDigit)
+		}
+	}
+	fmt.Println(rezString)
 
-	go download(s1, ch1)
-	go download(s2, ch2)
-	go download(s3, ch3)
-
-	//выведите сумму всех результатов
-	fmt.Println(<-ch1 + <-ch2 + <-ch3)
 }
